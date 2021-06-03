9aae32bf298de160744bb24ea142e68b96a7e1e1
Steve <info@konfer.be>
2021-06-03 11:44:38 +0200

    Test CI

--- test/e2e/00-bootstrap.e2e.test.js
+++ test/e2e/00-bootstrap.e2e.test.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
 
+
+require('dotenv').config();
+console.log('process.env', process.env);
 describe('E2e tests', () => {
   require('./01-smtp-mail-sending.e2e.test');
   require('./02-api-mail-sending.e2e.test');
