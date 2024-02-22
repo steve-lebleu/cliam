9d624266f2877b4d9f89e97b96bfe89664e9858b
Steve <info@konfer.be>
2024-02-22 14:51:27 +0100

    fix: cwd to retrieve config file

--- src/services/container.service.ts
+++ src/services/container.service.ts
@@ -1,13 +1,13 @@
 import * as Chalk from 'chalk';
 
-import { existsSync, readFileSync } from 'fs';
-
-import * as CliamCfg from './../../.cliamrc.js'
+import { existsSync } from 'fs';
 
 import { Transporter } from './../transporters/transporter.class';
 import { TransporterFactory } from './../transporters/transporter.factory';
 import { ClientConfiguration } from './../classes/client-configuration.class';
 import { configurationSchema } from './../validations/configuration.validation';
+
+const CliamConfiguration = require(process.cwd() + '/.cliamrc.js');
 
 /**
  * @description This class act like ... a container. She's a singleton, instanciated only one time when the application is ran. Mostly, she's responsible of:
@@ -65,7 +65,7 @@ class Container {
       process.stdout.write( Chalk.bold.red('.cliamrc.js file cannot be found\n') );
       process.exit(0);
     }
-    this.configuration = new ClientConfiguration( this.validates( CliamCfg ) );
+    this.configuration = new ClientConfiguration( this.validates( CliamConfiguration ) );
 
     this.transporters = this.configuration.transporters.reduce((result, transporterDefinition) => {
       result[transporterDefinition.id] = TransporterFactory.get(this.configuration.variables, transporterDefinition);
@@ -85,7 +85,7 @@ class Container {
   private validates(configuration: Record<string,unknown>): Record<string,unknown> {
     const error = configurationSchema.validate(configuration, { abortEarly: true, allowUnknown: false })?.error;
     if (error) {
-      process.stdout.write( Chalk.bold.red(`Error in .cliamrc.json: ${error.details.shift().message}\n`) );
+      process.stdout.write( Chalk.bold.red(`Error in .cliamrc.js: ${error.details.shift().message}\n`) );
       process.exit(0);
     }
     return configuration;
